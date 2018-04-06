6ccf793092192c55288273b364566ddd343b5306
xiaosean <xiaosean5408@gmail.com>
2018-04-06 23:15:51 +0800

    first commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+IOS套件管理-Swift Project Manager (SPM)教學
+
+可參考下面文章
+
+http://www.xiaosean.website/ios/2018/04/06/IOS-package-manager/
--- test_SPM/ViewController.swift
+++ test_SPM/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
-
+import SwiftyBeaver
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let log = SwiftyBeaver.self
+
     }
 
     override func didReceiveMemoryWarning() {
